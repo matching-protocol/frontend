cfec9bcc8d900b24f699903079b3fa0a206c156b
7wei <cwhuang0523@gmail.com>
2022-02-26 16:47:01 +0800

    update Swap components

--- src/pages/Offer/index.tsx
+++ src/pages/Offer/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import Card from 'components/Card'
 import { Box } from '@mui/material'
 import LogoText from 'components/LogoText'
@@ -10,37 +10,53 @@ import Divider from 'components/Divider'
 import WarningCard from './WarningCard'
 import BiSwap from 'components/Swap/BiSwap'
 import { Currency } from 'constants/token/currency'
+import SelectCurrencyModal from 'components/Input/CurrencyInputPanel/SelectCurrencyModal'
+import useModal from 'hooks/useModal'
 
 export default function Offer() {
-  const [fromChain, setFromChain] = useState(null)
-  const [toChain, setToChain] = useState(null)
-  const [fromCurrency, setFromCurrency] = useState(null)
-  const [toCurrency, setToCurrency] = useState(null)
+  const [fromChain, setFromChain] = useState<Chain | null>(null)
+  const [toChain, setToChain] = useState<Chain | null>(null)
+  const [fromCurrency, setFromCurrency] = useState<Currency | null>(null)
+  const [toCurrency, setToCurrency] = useState<Currency | null>(null)
   const [fromValue, setFromValue] = useState('')
   const [toValue, setToValue] = useState('')
+  const { showModal } = useModal()
+
   const chainList = ChainList
   const currencyList: Currency[] = []
+
+  const onSelectFromCurrency = useCallback(() => {
+    showModal(<SelectCurrencyModal onSelectCurrency={currency => setFromCurrency(currency as Currency)} />)
+  }, [])
+
+  const onSelectToCurrency = useCallback(() => {
+    showModal(<SelectCurrencyModal onSelectCurrency={currency => setToCurrency(currency as Currency)} />)
+  }, [])
 
   return (
     <Box pt={68} display="grid" gap={20} maxWidth={828} width="100%">
       <Card width="100%" padding="24px 60px 44px">
         <LogoText logo={OfferIcon} text=" Make an Offer" size="32px" fontSize={36} fontWeight={700} />
-        <BiSwap
-          fromLabel={"You'll send"}
-          toLabel={"You'll reveive"}
-          fromChain={fromChain}
-          fromCurrency={fromCurrency}
-          fromValue={fromValue}
-          toChain={toChain}
-          toCurrency={toCurrency}
-          toValue={toValue}
-          chainList={chainList}
-          currencyList={currencyList}
-          onSelectFromChain={setFromChain}
-          onSelectToChain={setToChain}
-          onSelectFromCurrency={setFromCurrency}
-          onSelectToCurrency={setToCurrency}
-        />
+        <Box mt={32}>
+          <BiSwap
+            fromLabel={"You'll send"}
+            toLabel={"You'll reveive"}
+            fromChain={fromChain}
+            fromCurrency={fromCurrency}
+            fromValue={fromValue}
+            toChain={toChain}
+            toCurrency={toCurrency}
+            toValue={toValue}
+            chainList={chainList}
+            currencyList={currencyList}
+            onSelectFromChain={setFromChain}
+            onSelectToChain={setToChain}
+            onSelectFromCurrency={onSelectFromCurrency}
+            onSelectToCurrency={onSelectToCurrency}
+            onChangeFromValue={e => setFromValue(e.target.value)}
+            onChangeToValue={e => setToValue(e.target.value)}
+          />
+        </Box>
 
         <Divider style={{ marginTop: 48, marginBottom: 24 }} />
         {/* <InputLabel style={{ marginTop: 24 }}>Incentive</InputLabel> */}

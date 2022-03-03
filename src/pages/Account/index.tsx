0110d75b77a9a9d0b074ceb71ece96dfb8bef8f2
7wei <cwhuang0523@gmail.com>
2022-03-03 15:14:07 +0800

    add Account Tabs

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -3,6 +3,10 @@ import { Box } from '@mui/material'
 import LogoText from 'components/LogoText'
 import AccountIcon from 'assets/images/account-lg.png'
 import PlainTabs from 'components/Tabs/PlainTabs'
+import Dashboard from './Dashboard'
+import LiveOffer from './LiveOffer'
+import LiveTaskList from './LiveTaskList'
+import History from './History'
 
 enum Tab {
   DASHBOARD,
@@ -23,6 +27,10 @@ export default function Account() {
           <LogoText logo={AccountIcon} text="Account" size="32px" fontSize={36} fontWeight={700} />
           <PlainTabs titles={Tabs} current={tab} onChange={setTab} />
         </Box>
+        {tab === Tab.DASHBOARD && <Dashboard />}
+        {tab === Tab.LIVE_OFFER && <LiveOffer />}
+        {tab === Tab.LIVE_TASK && <LiveTaskList />}
+        {tab === Tab.HISTORY && <History />}
       </Box>
     </>
   )

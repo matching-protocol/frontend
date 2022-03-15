d4816b8f610b3a5c2ebb0e9aef94b45727cace95
tang <1160224892@qq.com>
2022-03-15 14:25:00 +0800

    fix: update

--- src/components/Header/MobileHeader.tsx
+++ src/components/Header/MobileHeader.tsx
@@ -3,7 +3,7 @@ import { ExpandMore, Menu, Close } from '@mui/icons-material'
 import { NavLink } from 'react-router-dom'
 import { Box, MenuItem, AppBar, styled, Theme } from '@mui/material'
 import Modal from 'components/Modal'
-import ChainSwap from '../../assets/svg/chain_swap.svg'
+import BrandLogo from 'assets/svg/matching_protocol.svg'
 import { ExternalLink } from 'theme/components'
 import { ShowOnMobile } from 'theme/index'
 import Image from 'components/Image'
@@ -111,8 +111,8 @@ export default function MobileHeader() {
       <ShowOnMobile breakpoint="md">
         <StyledAppBar>
           <Box display="flex" alignItems="center">
-            <MainLogo id={'chainswap'} to={'/'}>
-              <Image src={ChainSwap} alt={'chainswap'} />
+            <MainLogo id={'logo'} to={'/'}>
+              <Image src={BrandLogo} alt={'logo'} />
             </MainLogo>
           </Box>
           {isOpen ? (
